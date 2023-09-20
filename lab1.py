12026e5a9b5274cafcae50400e4b28eea29b0103
Lauren Herman <thisisjustfabulous.0@gmail.com>
2023-09-20 17:09:17 -0600

    Change to raw

--- lab1.py
+++ lab1.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/LHERMAN5/Lab1/main/lab1.py?token=GHSAT0AAAAAACHR3TSY5IE3D4ZULCY2WDFIZILPNRA")
 
 print(resp.text)
